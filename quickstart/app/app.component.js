58846adae2dd9aa42cd02ae74052305e38c4bb30
Joanna Nowak <j.traciak@gmail.com>
2017-01-14 12:32:10 +0100

    list of students

--- quickstart/app/app.component.js
+++ quickstart/app/app.component.js
@@ -9,14 +9,40 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var Student = (function () {
+    function Student() {
+    }
+    return Student;
+}());
+exports.Student = Student;
+var STUDENTS = [
+    { id: 1, name: 'Mr. Nice' },
+    { id: 2, name: 'Narco' },
+    { id: 3, name: 'Bombasto' },
+    { id: 4, name: 'Celeritas' },
+    { id: 5, name: 'Magneta' },
+    { id: 6, name: 'RubberMan' },
+    { id: 7, name: 'Dynama' },
+    { id: 8, name: 'Dr IQ' },
+    { id: 9, name: 'Magma' },
+    { id: 10, name: 'Tornado' }
+];
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
+        this.title = 'Akademia 108 - lista studentow';
+        this.students = STUDENTS;
+        this.student = {
+            id: 1,
+            name: "Juri Kuzmoczka" };
     }
+    AppComponent.prototype.onSelect = function (student) {
+        this.selectedStudent = student;
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>Hello {{name}}</h1>",
+            template: "\n\t<h1>{{title}}</h1>\n    <h2>Students</h2>\n    <ul class=\"heroes\">\n      <li *ngFor=\"let student of students\"\n        [class.selected]=\"student === selectedStudent\"\n        (click)=\"onSelect(student)\">\n        <span class=\"badge\">{{student.id}}</span> {{student.name}}\n      </li>\n    </ul>\n    <div *ngIf=\"selectedStudent\">\n      <h2>{{selectedStudent.name}} details!</h2>\n      <div><label>id: </label>{{selectedStudent.id}}</div>\n      <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"selectedStudent.name\" placeholder=\"name\"/>\n      </div>\n    </div>\n\t\n\t",
+            styles: ["\n  .selected {\n    background-color: #CFD8DC !important;\n    color: white;\n  }\n  .heroes {\n    margin: 0 0 2em 0;\n    list-style-type: none;\n    padding: 0;\n    width: 15em;\n  }\n  .heroes li {\n    cursor: pointer;\n    position: relative;\n    left: 0;\n    background-color: #EEE;\n    margin: .5em;\n    padding: .3em 0;\n    height: 1.6em;\n    border-radius: 4px;\n  }\n  .heroes li.selected:hover {\n    background-color: #BBD8DC !important;\n    color: white;\n  }\n  .heroes li:hover {\n    color: #607D8B;\n    background-color: #DDD;\n    left: .1em;\n  }\n  .heroes .text {\n    position: relative;\n    top: -3px;\n  }\n  .heroes .badge {\n    display: inline-block;\n    font-size: small;\n    color: white;\n    padding: 0.8em 0.7em 0 0.7em;\n    background-color: #607D8B;\n    line-height: 1em;\n    position: relative;\n    left: -1px;\n    top: -4px;\n    height: 1.8em;\n    margin-right: .8em;\n    border-radius: 4px 0 0 4px;\n  }\n"]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- quickstart/app/app.component.ts
+++ quickstart/app/app.component.ts
@@ -1,7 +1,109 @@
 import { Component } from '@angular/core';
+export class Student{
+	id: number;
+	name: string;
+}
+
+const STUDENTS: Student[] = [
+  { id: 1, name: 'Mr. Nice' },
+  { id: 2, name: 'Narco' },
+  { id: 3, name: 'Bombasto' },
+  { id: 4, name: 'Celeritas' },
+  { id: 5, name: 'Magneta' },
+  { id: 6, name: 'RubberMan' },
+  { id: 7, name: 'Dynama' },
+  { id: 8, name: 'Dr IQ' },
+  { id: 9, name: 'Magma' },
+  { id: 10, name: 'Tornado' }
+];
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+	template: `
+	<h1>{{title}}</h1>
+    <h2>Students</h2>
+    <ul class="heroes">
+      <li *ngFor="let student of students"
+        [class.selected]="student === selectedStudent"
+        (click)="onSelect(student)">
+        <span class="badge">{{student.id}}</span> {{student.name}}
+      </li>
+    </ul>
+    <div *ngIf="selectedStudent">
+      <h2>{{selectedStudent.name}} details!</h2>
+      <div><label>id: </label>{{selectedStudent.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedStudent.name" placeholder="name"/>
+      </div>
+    </div>
+	
+	`
+	
+	styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
+
+
+	
+
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+	title = 'Akademia 108 - lista studentow'; 
+	students = STUDENTS;
+	student: Student = {
+		id: 1;
+		name: "Juri Kuzmoczka";};
+	selectedStudent: student;	
+	onSelect(student: Student): void {
+  	this.selectedStudent = student;
+}
+	
+}
+
